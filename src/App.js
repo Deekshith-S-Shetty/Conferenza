62cf7ee2591ca5b5af383f0a0177768466c9f92d
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-07 22:34:22 +0530

    Add routing for home and about, also initialize <BrowserRouter /> in index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>This is Home page</h1>} />
+        <Route path="/about" element={<h1>This is About page</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
