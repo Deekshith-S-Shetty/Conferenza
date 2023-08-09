f72dcba46f9228ffa0cff3c426e523a09a73ac24
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-09 13:25:36 +0530

    Add routes for Home, Room, About and ErrorPages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Room from "./pages/Room";
+import About from "./pages/About";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<h1>This is Home page</h1>} />
-        <Route path="/about" element={<h1>This is About page</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:id" element={<Room />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
