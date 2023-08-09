cfa0752a119517913cf2c0cf33c2a8c1514ed285
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-09 21:36:27 +0530

    Add About page for the appliation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
+import "./About.css";
+
 function About() {
   return (
     <>
-      <h1>Welcome to about page.</h1>
+      <div className="about">
+        <div className="about-image">
+          <img src="about.jpg" alt="loading" />
+        </div>
+        <div className="about-text">
+          <h1 className="about-heading">About Conferenza</h1>
+          <p className="about-para">
+            Conferenza is an all-in-one intelligent collaboration platform that
+            makes connecting easier, more immersive, and more dynamic for
+            businesses and individuals. Conferenza technology puts people at the
+            center, enabling meaningful connections, facilitating modern
+            collaboration, and driving human innovation through solutions like
+            team chat, phone, meetings, omnichannel cloud contact center, smart
+            recordings, whiteboard, and more, in one offering.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
