54f01019a6abda9ac44bc449fa267b26069407f9
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-09 19:51:44 +0530

    Create navbar and add background image for the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Room from "./pages/Room";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Room from "./pages/Room";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/room/:roomId" element={<Room />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </div>
