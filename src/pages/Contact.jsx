b5023afcb60ddb9ec4d8b384e07d299b99a08731
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-12 22:20:20 +0530

    Add html for contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,32 @@
+import "./Contact.css";
+
 function Contact() {
-  return <h1>Under Construction :)</h1>;
+  return (
+    <>
+      <div className="contact">
+        <h2 className="contact-heading">Send us a message</h2>
+        <form className="contact-form">
+          <div className="names">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="email">
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className="text-message">
+            <textarea
+              rows="8"
+              cols="99"
+              placeholder="Type your message here..."
+            ></textarea>
+          </div>
+          <div className="submit-button">
+            <input type="submit" value="submit" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
